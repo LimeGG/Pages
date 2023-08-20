bb1bb114dc6385bb9139fabd6e4995818bb203fd
LimeGG <setet97@gmail.com>
2023-08-20 21:41:01 +0300

    dr
    dr

--- page_for_girl/page_for_girl/settings.py
+++ page_for_girl/page_for_girl/settings.py
@@ -116,10 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    '/home/LimeGG/limegg.pythonanywhere.com/page_for_girl',
 ]
 
 # Default primary key field type
